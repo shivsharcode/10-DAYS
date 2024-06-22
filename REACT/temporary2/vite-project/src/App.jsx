2b59c776f8cdbf6c9ed1a661699c5a7243df22a9
shivsharcode <shivshar16@outlook.com>
2024-06-22 20:11:58 +0530

    css-training

---
+++ REACT/temporary2/vite-project/src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import "./css/style.css"
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  )
+}
---
+++ REACT/temporary2/vite-project/src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return(
+        <>
+            <header>
+                <h1>First React App</h1>
+            </header>
+        </>
+    )
+}
