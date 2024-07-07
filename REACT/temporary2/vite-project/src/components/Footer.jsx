9b56eafabf0b37d2d4a76422a4b6fc248e8921c7
shivsharcode <shivshar16@outlook.com>
2024-07-07 11:45:45 +0530

    deleted unused REACT FOLDER

--- REACT/temporary2/vite-project/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-    return(
-        <>
-            <footer className="footer-pg">
-                <div>footer</div>
-            </footer>
-        </>
-    )
-}
