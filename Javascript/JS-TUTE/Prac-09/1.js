50f90f43e09193f42695d99f927e863856f1e696
shivsharcode <shivshar16@outlook.com>
2024-07-14 23:49:10 +0530

    created Prac-09

---
+++ Javascript/JS-TUTE/Prac-09/1.js
@@ -0,0 +1,12 @@
+const student = {
+    fullName : "Shivam Sharma",
+    age : 20 ,
+    branch : "AIML" ,
+
+    // method
+    printAge: function(){
+        console.log("age : ", this.age) ;
+    }
+};
+
+
